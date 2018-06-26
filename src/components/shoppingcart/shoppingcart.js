588ab9546c272a312e89b21b4d481424e203708f
mojouy <santiago.mollajoli@neonroots.com>
2018-06-26 17:03:32 -0300

    Half of the tests running

--- src/components/shoppingcart/shoppingcart.js
+++ src/components/shoppingcart/shoppingcart.js
@@ -1,6 +1,11 @@
-import ShoppingcartModel from './shoppingcart.model';
+// import ShoppingcartModel from './shoppingcart.model';
+// // A different Approach using ES6 (didn't continue since wont work with current tests)
+// export function initShoppingCart() {
+//   let shoppingCart = new ShoppingcartModel();
+//   console.log(shoppingCart.init()); //eslint-disable-line no-console
+// }
+import Cart from './shoppingcart.model';
 
 export function initShoppingCart() {
-  let shoppingCart = new ShoppingcartModel();
-  console.log(shoppingCart.init()); //eslint-disable-line no-console
+  Cart.shoppingcartModel().init();
 }
--- src/components/shoppingcart/shoppingcart.model.js
+++ src/components/shoppingcart/shoppingcart.model.js
@@ -1,54 +1,92 @@
-'use strict';
+// 'use strict';
+// A different Approach using ES6 (didn't continue since wont work with current tests)
+//
+// export default class ShoppingcartModel {
+//   constructor() {
+//     console.log(window[NETCENTRIC_NAMESPACE])
+//   }
+//
+//   init() {
+//     return window[NETCENTRIC_NAMESPACE][MODULE_NAME];
+//   }
+// }
 
-export default class ShoppingcartModel {
-  constructor() {
-    this.constructor = this;
-  }
+(function() {
+  const MODULE_NAME = 'shoppingcartModel',
+    NETCENTRIC_NAMESPACE = 'nn';
 
-  init() {
-    return 99;
-  }
-}
+  window[NETCENTRIC_NAMESPACE] = window[NETCENTRIC_NAMESPACE] || {};
 
-// function shoppingcartModel() {
-//   var MODULE_NAME = 'shoppingcartModel',
-//       NETCENTRIC_NAMESPACE = 'nn';
-//
-//   window[NETCENTRIC_NAMESPACE] = window[NETCENTRIC_NAMESPACE] || {};
-//
-//   window[NETCENTRIC_NAMESPACE][MODULE_NAME] = function () {
-//     this._getCart = function _getCart() {
-//       return {
-//         total: {
-//           beforeVAT: 0,
-//           afterVAT: 0,
-//           VAT: 0
-//         },
-//         products: [],
-//         VATRate: null
-//       };
-//     };
-//
-//     return {
-//       init: function init() {
-//         return this._getCart();
-//       },
-//
-//       getCart: this._getCart,
-//
-//       addProducts: function addProducts(newOrExistingProducts) {
-//         return this.getCart( console.log("ss"));
-//       },
-//
-//       changeProductQuantity: function changeProductQuantity(product, newQuantity) {
-//         return _getCart();
-//       },
-//
-//       removeProducts: function removeProducts(productsToDelete) {
-//         return _getCart();
-//       },
-//
-//       destroy: function destroy() {}
-//     };
-//   };
-// };
+  window[NETCENTRIC_NAMESPACE][MODULE_NAME] = function() {
+    const _VATRate = 20;
+
+    const _getCart = function() {
+      return {
+        products: [],
+        total: {
+          beforeVAT: 0,
+          afterVAT: 0,
+          VAT: 0
+        }
+      };
+    };
+
+    const _getProductBeforeVAT = function(product) {
+      return product.price * product.quantity;
+    }
+
+    function _getVAT(beforeVAT, VATRate) {
+      return (beforeVAT / 100) * VATRate;
+    }
+
+    function _getAfterVAT(beforeVAT, VAT) {
+      return beforeVAT + VAT;
+    }
+
+    function _calculateTotals(product, total) {
+      let { beforeVAT, afterVAT } = total;
+      beforeVAT = _getProductBeforeVAT(product);
+      let VAT = _getVAT(beforeVAT, _VATRate);
+      afterVAT = _getAfterVAT(product.price, VAT);
+
+      return  {
+        beforeVAT,
+        afterVAT,
+        VAT
+      };
+    }//Calculate Totals (beforeVAT afterVAT VAT)
+
+    return {
+      init: function() {
+        return 99;
+      },
+
+      getCart: _getCart,
+
+      addProducts: function(newOrExistingProducts) {
+        let cart = _getCart();
+        let { products, total} = cart;
+
+        cart.total = _calculateTotals(newOrExistingProducts, total);
+        console.log(//eslint-disable-line no-console
+          products.filter(
+            product => product.name !== newOrExistingProducts.name));
+        products.push(newOrExistingProducts);
+        return cart;
+      },
+
+      changeProductQuantity: function() { //product, newQuantity
+        return _getCart();
+      },
+
+      removeProducts: function() { //productsToDelete
+        return _getCart();
+      },
+
+      destroy: function() {
+        return console.log('Destroy'); //eslint-disable-line no-console
+      },
+    };
+  };
+})();
+module.exports = window.nn;
