6e0009c29e30b3a6d1a3d79d510b09c683508b83
mojouy <santiago.mollajoli@neonroots.com>
2018-06-27 01:52:22 -0300

    Initial jquery hookers

--- src/components/shoppingcart/shoppingcart.js
+++ src/components/shoppingcart/shoppingcart.js
@@ -4,8 +4,37 @@
 //   let shoppingCart = new ShoppingcartModel();
 //   console.log(shoppingCart.init()); //eslint-disable-line no-console
 // }
+import $ from 'jquery';
 import Cart from './shoppingcart.model';
 
 export function initShoppingCart() {
-  Cart.shoppingcartModel().init();
+  const $addToCartBtn = $('.shop__item button');
+  const $shoppingCartForm = $('#shoppingcart-form');
+  const $shoppingCartVATS = $('.shoppingcart__taxes');
+  const $priceBeforeVAT = $shoppingCartVATS.find('.price-before-vat output');
+  const $VATPercentage = $shoppingCartVATS.find('.price-vat output');
+  const $priceAfterVAT = $shoppingCartVATS.find('.shoppingcart__total output');
+
+  function initCart() {
+    const cart = Cart.shoppingcartModel(),
+          total = cart.getCart().total,
+          beforeVAT = total.beforeVAT,
+          afterVAT = total.afterVAT,
+          VAT = total.VAT;
+
+    cart.init();
+    $priceBeforeVAT.text(beforeVAT);
+    $priceAfterVAT.text(afterVAT);
+    $VATPercentage.text(VAT);
+  }
+
+  $(document).ready(function() {
+    initCart();
+    $addToCartBtn.each((index, el) => {
+      $(el).click((event) => {
+        event.preventDefault();
+        console.log($(el).data('shop-listing')) //eslint-disable-line no-console
+      });
+    });
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
 import { initShoppingCart } from './components/shoppingcart/shoppingcart';
-initShoppingCart();//
+initShoppingCart();
